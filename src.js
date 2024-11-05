b8b3c87aefca07582d6f05a4cb39f70c4b34f0c9
Daniel <d.popov0470@gmail.com>
2024-11-05 20:27:25 +0200

    feat: add change grid size functionality and and extract grid styling into function

--- src.js
+++ src.js
@@ -1,24 +1,46 @@
-const container = document.querySelector(".container");
-const updateGridSizeButton = document.querySelector('button');
+const gridContainer = document.querySelector(".grid-container");
+const changeGridSizeButton = document.querySelector("#change-grid");
+const GRID_SIZE = 600;
 
 function createGrid(size = 16) {
-  let squareSize = 500 / size;
+  let squareSize = GRID_SIZE / size;
 
   for (let i = 0; i < size; i++) {
-    let row = document.createElement("div");
-    row.classList.add("row");
-  
+    let column = document.createElement("div");
+    column.classList.add("column");
+
     for (let j = 0; j < size; j++) {
       let gridItem = document.createElement("div");
-      gridItem.classList.add("grid-item");
-      gridItem.addEventListener('mouseenter', (e) => { e.target.classList.add('hover') });
-      gridItem.style.width = `${squareSize}px`;
-      gridItem.style.height = `${squareSize}px`;
-      row.appendChild(gridItem);
+      styleGridItem(gridItem, squareSize);
+      column.appendChild(gridItem);
     }
-    container.appendChild(row);
+    gridContainer.appendChild(column);
   }
 }
 
-createGrid();
+function styleGridItem(gridItem, squareSize) {
+  gridItem.classList.add("grid-item");
+  gridItem.addEventListener("mouseenter", (e) => {
+    e.target.classList.add("hover");
+  });
+  gridItem.style.width = `${squareSize}px`;
+  gridItem.style.height = `${squareSize}px`;
+  return gridItem;
+}
 
+function changeGridSize() {
+  let newGridSize = Number(prompt("Enter grid size - Maximum size 100"));
+
+  while (isNaN(newGridSize) || newGridSize > 100) {
+    alert("Invalid size. Please make sure you are entering a number below 100");
+    newGridSize = Number(prompt("Enter your desired grid size."));
+  }
+
+  if (newGridSize) {
+    gridContainer.textContent = '';
+    createGrid(newGridSize);
+  }
+}
+
+changeGridSizeButton.addEventListener("click", changeGridSize);
+createGrid();
